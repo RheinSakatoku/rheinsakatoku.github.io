d5e3cfd6e0f3b96be12fcce5e01930fc9f09ae45
Rhein <rheinsakatoku@gmail.com>
2025-06-17 23:46:50 +0300

    newstyle

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("project-button").addEventListener("click", () => {
+  const dropdown = document.getElementById("dropdown");
+  dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
+});
